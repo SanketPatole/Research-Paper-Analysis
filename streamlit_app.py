4c0d466352c5d1af0de169f86777a40343a5a127
SanketPatole <69233883+SanketPatole@users.noreply.github.com>
2024-03-21 13:04:59 +0530

    Fixing bug

--- streamlit_app.py
+++ streamlit_app.py
@@ -149,13 +149,13 @@ class GenAI_Wrpapper:
 		return response['answer']
 
 class Page:
-	research_paper_summary = None
 	def __init__(self):
 		self.research_paper_url = None
 		self.research_paper_content = ""
 		self.submit_object = None
 		self.submit_object2 = None
 		self.genai_wrapper_object = None
+		self.research_paper_summary = None
 	
 	def create_header(self, displayText="Header"):
 		st.header(displayText)
@@ -220,15 +220,15 @@ class Page:
 			elif len(self.research_paper_content.strip()) > 0:
 				try:
 					self.genai_wrapper_object = GenAI_Wrpapper(chat_client)
-					Page.research_paper_summary = self.get_summary()
+					self.research_paper_summary = self.get_summary()
 				except Exception as e1:
 					try:
 						self.genai_wrapper_object = GenAI_Wrpapper(alternative_model[chat_client])
-						Page.research_paper_summary = self.get_summary()
+						self.research_paper_summary = self.get_summary()
 					except Exception as e2:
 						self.create_error_message(displayText=f"Unble to connect to ChatBot at his moment. Please try again later.")
-		if Page.research_paper_summary:
-			self.display_research_paper_summary(Page.research_paper_summary)
+		if self.research_paper_summary:
+			self.display_research_paper_summary(self.research_paper_summary)
 			self.create_header(displayText="Ask a question.")
 			question = self.create_input_text(displayText="Paste your question here...", height=1)
 			self.submit_object2 = self.create_submit_button(displayText="Ask")
